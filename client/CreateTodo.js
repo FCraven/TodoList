9b8f7b951e323bbd6033b553c80b154d31f59be0
FCraven <craven.francis@gmail.com>
2019-01-15 14:09:08 -0500

    form fields added to createTodo

--- client/CreateTodo.js
+++ client/CreateTodo.js
@@ -3,7 +3,16 @@ import React, {Component} from 'react'
 export default class CreateTodo extends Component {
   render () {
     return (
-      <form></form>
+      <form>
+
+        <label htmlFor='taskName'>Task Name:</label>
+        <input type='text' name='taskName' placeholder='Task Name'></input>
+
+        <label htmlFor='assignee'>Assignee:</label>
+        <input type='text' name='assignee' placeholder='Assigned to...'></input>
+
+        <button type='submit'>Submit</button>
+      </form>
     )
   }
 }
--- server/api/todos.js
+++ server/api/todos.js
@@ -1,18 +1,21 @@
 const router = require('express').Router()
 const {Todo} = require('../db')
 
+//GET /api/todos
 router.get('/', (req, res, next) => {
   Todo.findAll()
     .then(todos => res.json(todos))
     .catch(next)
 })
 
+//GET /api/todos/:todoId
 router.get('/:todoId', (req, res, next) => {
   Todo.findById(req.params.todoId)
     .then(todo => res.json(todo))
     .catch(next)
 })
 
+//POST /api/todos
 router.post('/', (req, res, next) => {
   Todo.create(req.body)
     .then(todo => res.json(todo))
